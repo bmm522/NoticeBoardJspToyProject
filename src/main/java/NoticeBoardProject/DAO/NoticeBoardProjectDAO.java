2f32a76bfe7527f1eb766e89df8345905b7f2196
JiIn Kim <rlawldls11@naver.com>
2022-07-17 21:51:10 +0900

    삭제 구현안되는 버그 해결

--- src/main/java/NoticeBoardProject/DAO/NoticeBoardProjectDAO.java
+++ src/main/java/NoticeBoardProject/DAO/NoticeBoardProjectDAO.java
@@ -149,7 +149,8 @@ public abstract class NoticeBoardProjectDAO {
 	
 	public void deleteInDatabase(Connection con, PreparedStatement pst, int id) {
 		try {
-			pst.setInt(1, id);			
+			pst.setInt(1, id);
+			pst.executeUpdate();
 		}catch(SQLException e){
 			System.out.println("deleteInDatabase¿À·ù");
 		}finally {
