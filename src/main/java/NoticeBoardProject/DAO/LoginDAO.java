ce2428f5485d35622886a2383b7e239b2d4bcdbd
JiIn Kim <rlawldls11@naver.com>
2022-09-13 15:35:55 +0900

    로그인 CodeEncryption 적용 완료

--- src/main/java/NoticeBoardProject/DAO/LoginDAO.java
+++ src/main/java/NoticeBoardProject/DAO/LoginDAO.java
@@ -6,38 +6,83 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import NoticeBoardProject.DAO.Token.Token;
+import codeEncryption.oneWay.action.CheckMachingCode;
 
 public class LoginDAO extends NoticeBoardProjectDAO{
 
 	
 	public Token getTokenOfLoginCheck(String userId, String userPwd)  {
-			String sql = "SELECT USERPWD FROM BOARDMEMBER WHERE USERID=? AND USERPWD=?";
+//			String sql = "SELECT USERPWD FROM BOARDMEMBER WHERE USERID=? AND USERPWD=?";
 			Connection con = ConnectionDriver();
 			PreparedStatement pst = null;
 			ResultSet rs = null;
-			try {
-				pst = getLoginPst(con.prepareStatement(sql), sql, userId, userPwd);
-				rs = pst.executeQuery();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			return getTokenOfLoginCheckIfLogic(con, pst ,rs);
+			
+//			checkPwd(con, pst, rs, userId, userPwd);
+//			try {
+//				pst = getLoginPst(con.prepareStatement(sql), sql, userId, userPwd);
+//				rs = pst.executeQuery();
+//			} catch (SQLException e) {
+//				e.printStackTrace();
+//			}
+			
+			return getTokenOfLoginCheckIfLogic(con, pst ,rs, userId, userPwd);
 		}
 	
 
 
 
-	public Token getTokenOfLoginCheckIfLogic(Connection con, PreparedStatement pst, ResultSet rs) {
-		try {	
-			if(rs.next()) 
-				return Token.LOGINSUCCESS;
-				
-			return Token.LOGINFAIL;
+	private boolean checkPwd(Connection con, PreparedStatement pst, ResultSet rs, String userId, String userPwd) {
+		CheckMachingCode cmc = new CheckMachingCode();
+		String salt = getSalt(con, pst, rs, userId);
+		return cmc.checkMachingCode(userId, userPwd, salt, getHashCode(con,pst,rs,userId));
+		
+	}
+
+	private String getSalt(Connection con, PreparedStatement pst, ResultSet rs, String userId) {
+		String sql = "SELECT * FROM MEMBERIDCODE WHERE MEMBERID = ?";
+		String salt = "";
+		try {
+			pst = con.prepareStatement(sql);
+			pst.setString(1, userId);
+			rs = pst.executeQuery();
+			if(rs.next()) {
+				salt = rs.getString("SALT");
+			}
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
-			} finally {
-				 JdbcClose(con, pst, rs); //강제로 예외발생시켜서 닫게함
+		}
+		return salt;	
+	}
+
+
+	private String getHashCode(Connection con, PreparedStatement pst, ResultSet rs, String userId) {
+		String sql = "SELECT * FROM BOARDMEMBER WHERE USERID = ?";
+		String hashCode = "";
+		try {
+			pst = con.prepareStatement(sql);
+			pst.setString(1, userId);
+			rs = pst.executeQuery();
+			if(rs.next()) {
+				hashCode = rs.getString("USERPWD");
 			}
-			return Token.LOGINERROR;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return hashCode;
+	}
+
+
+
+
+
+	public Token getTokenOfLoginCheckIfLogic(Connection con, PreparedStatement pst, ResultSet rs,String userId, String userPwd) {
+			
+			if(checkPwd(con,pst,rs,userId,userPwd)) {
+				JdbcClose(con, pst, rs);
+				return Token.LOGINSUCCESS;
+			}
+			return Token.LOGINFAIL;
+		 
 		}
 }
